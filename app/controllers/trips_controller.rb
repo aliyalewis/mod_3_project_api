dd0f9bb79a915e877168b4634a417c111d736355
Aliya <48336821+aliyalewis@users.noreply.github.com>
2019-08-02 11:23:16 -0700

    Revert "New branch"

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,24 +1,8 @@
 class TripsController < ApplicationController
 
   def index
-    trips = Trip.all
-    render json: trips
-  end
-
-  def create
-    trip = Trip.create(id: params[:id], name: params[:name], status: params[:status], likes: 0, review: "", user_id: params[:user_id], country_id: params[:country_id])
-    render json: trip
-  end
-
-  def update
-    trip = Trip.find(params[:id])
-    trip.update(status: params[:status], likes: params[:likes], review: params[:review])
-    render json: trip
-  end
-
-  def destroy
-    trip = Trip.find(params[:id])
-    trip.delete
+    @trips = Trip.all
+    render json: @trips
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,4 @@ Rails.application.routes.draw do
   resources :countries
   resources :trips
   resources :users
-
-  get 'countries/:id/rev', to: 'countries#reviews'
 end
